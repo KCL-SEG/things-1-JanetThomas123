00a15b724e517cb58ea8860f37366f3cd84fd870
JanetThomas123 <115407383+JanetThomas123@users.noreply.github.com>
2022-12-08 23:19:36 +0000

    Delete models.py

--- thingproject/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Thing(models.Model):
-    name = models.CharField(unique=true,blank=False,max_length=30)
-    description = models.CharField(unique=False,max_length=120)
-    quantity = models.PositiveIntegerField(unique=True,validators=[MinValueValidator(1), MaxValueValidator(100)])
